45f2c006988ea463d63f4b5a1e87d7317358a835
GToldy <georgina.toldy@gmail.com>
2022-11-09 17:53:13 +0100

    Add Util class

---
+++ src/test/java/Util.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Util {
+    public static WebElement lookUpWebElementWithWait(WebDriver driver, String selector) {
+        return new WebDriverWait(driver, Duration.ofMillis(1000)).until(ExpectedConditions.elementToBeClickable(By.cssSelector(selector)));
+    }
+
+    public static WebElement lookUpWebElementWithWait(WebDriver driver, By element) {
+        return new WebDriverWait(driver, Duration.ofMillis(1000)).until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static WebElement lookUpWebElementWithWait(WebDriver driver, int duration, String selector) {
+        return new WebDriverWait(driver, Duration.ofMillis(duration)).until(ExpectedConditions.elementToBeClickable(By.cssSelector(selector)));
+    }
+
+    public static By lookUpWebElementByRelativeLocator(String parent, String child) {
+        return RelativeLocator.with(By.cssSelector(parent)).below(By.cssSelector(child));
+    }
+
+    public static void scrollToElement(WebDriver driver, WebElement element) {
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("arguments[0].scrollIntoView()", element);
+    }
+
+    public static void logOut(WebDriver driver) {
+        WebElement userProfile = driver.findElement(By.id("header-details-user-fullname"));
+        Util.scrollToElement(driver, userProfile);
+        userProfile.click();
+        WebElement logOutOption = driver.findElement(By.id("log_out"));
+        logOutOption.click();
+    }
+}
