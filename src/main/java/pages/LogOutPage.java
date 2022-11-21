517bc00bbbb99c3ed6e7d69bf880bb879bac747e
Kozma Gábor <kozma.jr@gmail.com>
2022-11-21 12:37:15 +0100

    Add class for logout page objects

---
+++ src/main/java/pages/LogOutPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LogOutPage {
+
+    WebDriver driver;
+
+    @FindBy(xpath = "//*[@class='title']/child::*[1]")
+    WebElement logOutMessage;
+
+
+    public LogOutPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getLogOutMessage() {
+        return logOutMessage.getText();
+    }
+}
