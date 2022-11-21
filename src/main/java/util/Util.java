8926d793be50149ee5e6e517a8b88a34a00f6039
Kozma Gábor <kozma.jr@gmail.com>
2022-11-21 11:37:35 +0100

    Add methods for setting up drivers.

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,9 +1,11 @@
 package util;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,6 +13,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Util {
+
+    public static WebDriver driver;
+
     public static WebElement lookUpWebElementWithWait(WebDriver driver, String selector) {
         return new WebDriverWait(driver, Duration.ofMillis(1000)).until(ExpectedConditions.elementToBeClickable(By.cssSelector(selector)));
     }
@@ -41,7 +46,16 @@ public class Util {
         logOutOption.click();
     }
 
-    public void navigateToUrl(WebDriver driver, String url) {
+    public static void navigateToUrl(WebDriver driver, String url) {
         driver.get(url);
+    }
+
+
+    public static void setChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    public static void getChromeDriver() {
+        driver = new ChromeDriver();
     }
 }
