b1a0528cc0c7a3b620e55eb319a0895ea7334e7e
Kozma Gábor <kozma.jr@gmail.com>
2022-11-21 11:38:23 +0100

    Modify wait method

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -16,8 +16,8 @@ public class Util {
 
     public static WebDriver driver;
 
-    public static WebElement lookUpWebElementWithWait(WebDriver driver, String selector) {
-        return new WebDriverWait(driver, Duration.ofMillis(1000)).until(ExpectedConditions.elementToBeClickable(By.cssSelector(selector)));
+    public static WebElement lookUpWebElementWithWait(WebDriver driver, WebElement element) {
+        return new WebDriverWait(driver, Duration.ofMillis(1000)).until(ExpectedConditions.elementToBeClickable(element));
     }
 
     public static WebElement lookUpWebElementWithWait(WebDriver driver, By element) {
@@ -38,13 +38,13 @@ public class Util {
     }
 
 
-    public static void logOut(WebDriver driver) {
+/*    public static void logOut(WebDriver driver) {
         WebElement userProfile = lookUpWebElementWithWait(driver, "#header-details-user-fullname");
         Util.scrollToElement(driver, userProfile);
         userProfile.click();
         WebElement logOutOption = driver.findElement(By.id("log_out"));
         logOutOption.click();
-    }
+    }*/
 
     public static void navigateToUrl(WebDriver driver, String url) {
         driver.get(url);
