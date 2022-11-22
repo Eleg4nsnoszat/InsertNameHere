48f04dc4d9143cc36a2f500bdce98b5629e5fa54
Kozma Gábor <kozma.jr@gmail.com>
2022-11-22 12:08:51 +0100

    Refactor remove WebElements and methods that are defined in Dashboard

--- src/main/java/pages/ProjectSummaryPage.java
+++ src/main/java/pages/ProjectSummaryPage.java
@@ -16,11 +16,6 @@ public class ProjectSummaryPage {
     @FindBy(xpath = "//*[@id='main']/*[contains(@class, 'page-heading')]")
     WebElement cannotViewProjectMessage;
 
-    @FindBy(xpath = "//*[@id='header-details-user-fullname']")
-    WebElement userProfile;
-
-    @FindBy(xpath = "//*[@id='log_out']")
-    WebElement logOut;
 
 
 
@@ -38,13 +33,5 @@ public class ProjectSummaryPage {
 
     public String getCannotViewProjectMessage() {
         return cannotViewProjectMessage.getText();
-    }
-
-    public WebElement getUserProfile() {
-        return userProfile;
-    }
-
-    public WebElement getLogOut() {
-        return logOut;
     }
 }
