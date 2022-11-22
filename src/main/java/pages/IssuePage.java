d1b5e2d6f47591e8f8cd5878cc1676ba4b66216c
Kozma Gábor <kozma.jr@gmail.com>
2022-11-22 10:34:17 +0100

    Add webelements and methods for handling issues

--- src/main/java/pages/IssuePage.java
+++ src/main/java/pages/IssuePage.java
@@ -2,9 +2,12 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import util.Util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IssuePage {
 
@@ -18,6 +21,18 @@ public class IssuePage {
 
     @FindBy(xpath = "//*[@id='log_out']")
     WebElement logOut;
+
+    @FindBy(xpath = "//*[@id='summary-val']")
+    WebElement issueSummaryHeader;
+
+    @FindBy(xpath = "//*[@id='opsbar-operations_more']")
+    WebElement moreOptionsDropdown;
+
+    @FindBy(xpath = "//*[@id='delete-issue']/a")
+    WebElement deleteOption;
+
+    @FindBy(xpath = "//*[@id='delete-issue-submit']")
+    WebElement confirmDelete;
 
 
     public IssuePage(WebDriver driver) {
@@ -36,5 +51,20 @@ public class IssuePage {
 
     public WebElement getLogOut() {
         return logOut;
+    }
+
+    public void validateIssueBase(String issueSummary) {
+        WebElement issueSummaryTheSame = Util.lookUpWebElementWithWait(driver, issueSummaryHeader);
+        assertEquals(issueSummary, issueSummaryTheSame.getText());
+    }
+
+    public void deleteIssue() {
+        Actions actions = new Actions(driver);
+        WebElement moreButton = Util.lookUpWebElementWithWait(driver, moreOptionsDropdown);
+        actions.moveToElement(moreButton).click().build().perform();
+        WebElement deleteButton = Util.lookUpWebElementWithWait(driver, deleteOption);
+        deleteButton.click();
+        WebElement confirmDeleteButton = Util.lookUpWebElementWithWait(driver, confirmDelete);
+        confirmDeleteButton.click();
     }
 }
